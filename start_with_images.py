8120cb08230d9ee81af770be44bb6a6995a9afa7
kye <kai.ye@marklogic.com>
2020-09-29 09:54:55 -0700

    Add comments

--- start_with_images.py
+++ start_with_images.py
@@ -1,10 +1,11 @@
 import cv2
 
 img = cv2.imread('lena.jpg', -1)
-# 1 is to load color image, 0 is to load grayimage, -1 is to load unchanged image
+# 1 is to load color image, 0 is to load grayscale image, -1 is to load unchanged image
 cv2.imshow('image', img)
 # 'image' is window name
 k = cv2.waitKey(0) & 0xFF
+# 0 mill seconds, infinite wait
 # 0xFF is mask used for 64 bits OS
 
 if k == 27:
