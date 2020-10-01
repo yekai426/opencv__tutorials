227872ff4b3ce657b63b0935d25cc5146f7ed5cb
kye <kai.ye@marklogic.com>
2020-10-01 15:33:22 -0700

    Update blue, green, red coordinates

--- mouse_event.py
+++ mouse_event.py
@@ -12,9 +12,9 @@ def click_event(event, x, y, flags, param):
         cv2.putText(img, strXY, (x, y), font, .5, (255, 255, 0), 2)
         cv2.imshow('image', img)
     if event == cv2.EVENT_RBUTTONDOWN:
-        blue = img[y, x, 0]
-        green = img[y, x, 1]
-        red = img[y, x, 2]
+        blue = img[x, y, 0]
+        green = img[x, y, 1]
+        red = img[x, y, 2]
         font = cv2.FONT_HERSHEY_SIMPLEX
         strBGR = str(blue) + ', '+ str(green)+ ', '+ str(red)
         cv2.putText(img, strBGR, (x, y), font, .5, (0, 255, 255), 2)
