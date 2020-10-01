0a47810a619f0fbcb6c8aac4850a855978b29db9
kye <kai.ye@marklogic.com>
2020-10-01 15:47:13 -0700

    Add comment

--- mouse_event_draw_function.py
+++ mouse_event_draw_function.py
@@ -5,17 +5,17 @@ def click_event(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
         cv2.circle(img, (x, y), 3, (0, 0, 255), -1)
         points.append((x, y))
-        if len(points) >=2:
+        if len(points) >= 2:
             cv2.line(img, points[-1], points[-2], (255, 0, 0), 5)
         cv2.imshow('image', img)
     if event == cv2.EVENT_RBUTTONDOWN:
         blue = img[x, y, 0]
         green = img[x, y, 1]
         red = img[x, y, 2]
-        mycolor_image = np.zeros((512, 512, 3), np.uint8)
+        color_image = np.zeros((512, 512, 3), np.uint8)
         # 512, 512 is windows size, 3 is channels
-        mycolor_image[:] = [blue, green, red]
-        cv2.imshow('color', mycolor_image)
+        color_image[:] = [blue, green, red]
+        cv2.imshow('color', color_image)
 
 img = cv2.imread('lena.jpg')
 cv2.imshow('image', img)
