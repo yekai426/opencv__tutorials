f43082d011f30186dd870cf5897879bb60708bea
kye <kai.ye@marklogic.com>
2020-10-04 16:26:02 -0700

    Add comment

--- adaptive_thresholding.py
+++ adaptive_thresholding.py
@@ -4,9 +4,9 @@ import numpy as np
 img = cv.imread('sudoku.png',0)
 _, th1 = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
 th2 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
-# cv.ADAPTIVE_THRESH_MEAN_C is adaptive method, cv.THRESH_BINARY is threshold type, 11 is block siez, 2 is C value
+# cv.ADAPTIVE_THRESH_MEAN_C is adaptive method, cv.THRESH_BINARY is threshold type, 11 is block size, 2 is C value
 th3 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
-# cv.ADAPTIVE_THRESH_GAUSSIAN_C is adaptive method, cv.THRESH_BINARY is threshold type, 11 is block siez, 2 is C value
+# cv.ADAPTIVE_THRESH_GAUSSIAN_C is adaptive method, cv.THRESH_BINARY is threshold type, 11 is block size, 2 is C value
 
 cv.imshow("Image", img)
 cv.imshow("THRESH_BINARY", th1)
