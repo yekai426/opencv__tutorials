b14ee7276abc50a2420d25bbae6e4d955f62d2d5
kye <kai.ye@marklogic.com>
2020-10-08 16:03:49 -0700

    Find and draw contours

---
+++ find_and_draw_contours.py
@@ -0,0 +1,23 @@
+"""
+Find contours  and draw contours
+The function retrieves contours from the binary image.
+The contours are a useful tool for shape analysis and object detection and recognition.
+"""
+import numpy as np
+import cv2
+
+img = cv2.imread('opencv-logo.png')
+imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(imgray, 100, 255, 0)
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+print("Number of contours = " + str(len(contours)))
+#print(contours[0])
+
+cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
+# -1 is contours index and means all contours, (0, 255, 0) is color, 3 is thickness
+cv2.drawContours(imgray, contours, -1, (0, 255, 0), 3)
+
+cv2.imshow('Image', img)
+cv2.imshow('Image GRAY', imgray)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
