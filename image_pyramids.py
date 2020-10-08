ac7bbe6b369b30635854f0025a15f96031bac01c
kye <kai.ye@marklogic.com>
2020-10-07 17:21:35 -0700

    Add comment

--- image_pyramids.py
+++ image_pyramids.py
@@ -13,15 +13,14 @@ for i in range(6):
     gaussian_pyramid_list.append(layer)
     #cv2.imshow(str(i), layer)
 
-layer = gaussian_pyramid_list[6]
-#cv2.imshow('upper level Gaussian Pyramid', layer)
+layer = gaussian_pyramid_list[5]
+cv2.imshow('upper level Gaussian Pyramid', layer)
 laplacian_pyramid_list = [layer]
 
-for i in range(6, 0, -1):
+for i in range(5, 0, -1):
     gaussian_extended = cv2.pyrUp(gaussian_pyramid_list[i])
     laplacian = cv2.subtract(gaussian_pyramid_list[i-1], gaussian_extended)
     cv2.imshow(str(i), laplacian)
-
 
 cv2.imshow("Original image", img)
 cv2.waitKey(0)
