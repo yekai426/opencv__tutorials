c91c0f0952b9cf7c61eaee2008e9e733561f0b87
kye <kai.ye@marklogic.com>
2020-10-09 16:37:37 -0700

    Add comment

--- use_histograms_with_calHist.py
+++ use_histograms_with_calHist.py
@@ -6,12 +6,14 @@ import cv2 as cv
 from matplotlib import pyplot as plt
 
 img = cv.imread("lena.jpg", 0)
+# read grayscale image
 #img = np.zeros((200,200), np.uint8)
 #cv.rectangle(img, (0, 100), (200, 200), (255), -1)
 #cv.rectangle(img, (0, 50), (100, 100), (127), -1)
 
 
 hist = cv.calcHist([img], [0], None, [256], [0, 256])
+# [0] is 0 channel
 plt.plot(hist)
 plt.show()
 
