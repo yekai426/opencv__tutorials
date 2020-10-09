4def6ce50983ac8b77c058f7a68e3defc387b55c
kye <kai.ye@marklogic.com>
2020-10-09 16:31:45 -0700

    How to use opencv Histograms function.

---
+++ use_histograms_with_calHist.py
@@ -0,0 +1,19 @@
+"""
+How to use opencv Histograms function.
+"""
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+img = cv.imread("lena.jpg", 0)
+#img = np.zeros((200,200), np.uint8)
+#cv.rectangle(img, (0, 100), (200, 200), (255), -1)
+#cv.rectangle(img, (0, 50), (100, 100), (127), -1)
+
+
+hist = cv.calcHist([img], [0], None, [256], [0, 256])
+plt.plot(hist)
+plt.show()
+
+cv.waitKey(0)
+cv.destroyAllWindows()
