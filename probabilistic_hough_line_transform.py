8a2319ac61e9eda51a8ed63af1491ece40d96c2d
kye <kai.ye@marklogic.com>
2020-10-10 17:26:13 -0700

    Add comment

--- probabilistic_hough_line_transform.py
+++ probabilistic_hough_line_transform.py
@@ -3,6 +3,7 @@ Probabilistic Hough Transform using HoughLinesP method in OpenCV.
 """
 import cv2
 import numpy as np
+
 img = cv2.imread('sudoku.png')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray, 50, 150, apertureSize=3)
