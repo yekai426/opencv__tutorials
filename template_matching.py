a9afcd1d3c795477ff5930d40287f11289b129d3
kye <kai.ye@marklogic.com>
2020-10-10 17:23:41 -0700

    Match template in a image

--- template_matching.py
+++ template_matching.py
@@ -3,17 +3,19 @@ Template Matching is a method for searching and finding the location of a templa
 """
 import cv2
 import numpy as np
+
 img = cv2.imread("messi5.jpg")
 grey_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 template = cv2.imread("messi_face.jpg", 0)
 w, h = template.shape[::-1]
-# [::-1] is to reverse row, column order to column and row
+# shape return row and column. [::-1] is to reverse row, column order to column and row
 
-res = cv2.matchTemplate(grey_img, template, cv2.TM_CCORR_NORMED )
+res = cv2.matchTemplate(grey_img, template, cv2.TM_CCORR_NORMED)
 print(res)
 threshold = 0.99
 loc = np.where(res >= threshold)
 print(loc)
+
 for pt in zip(*loc[::-1]):
 # * is to create tuple, [::-1] is to reverse row, column order to column and row
     cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
