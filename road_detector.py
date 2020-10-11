d03512fcdfe2c157c9bcccde8a4547b36cea2bfc
kye <kai.ye@marklogic.com>
2020-10-11 16:34:42 -0700

    Find the region of interest from road picture

---
+++ road_detector.py
@@ -0,0 +1,35 @@
+"""
+Find the region of interest from road picture
+"""
+
+import matplotlib.pylab as plt
+import cv2
+import numpy as np
+
+image = cv2.imread('road.jpg')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+print(image.shape)
+height = image.shape[0]
+width = image.shape[1]
+
+region_of_interest_vertices = [
+    (0, height),
+    (width/2, height/2),
+    (width, height)
+]
+
+def region_of_interest(img, vertices):
+    mask = np.zeros_like(img)
+    channel_count = img.shape[2]
+    match_mask_color = (255,) * channel_count
+    cv2.fillPoly(mask, vertices, match_mask_color)
+    # fill vertices area with match_mask_color in mask image
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
+
+cropped_image = region_of_interest(image,
+                np.array([region_of_interest_vertices], np.int32),)
+
+plt.imshow(cropped_image)
+plt.show()
